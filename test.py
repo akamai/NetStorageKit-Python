5cb622378aead7e2bd49bad39ee3d2fd8e18e34e
astinchoi <asciineo@gmail.com>
2016-04-05 00:56:42 +0900

    add downlaod

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from akamai.netstorage import Netstorage
 
 
-ns = Netstorage("astin-nsu.akamaihd.net", "astinastin" ,"NIaOox2Z41Qf975MO2v9x78MC75HJA35Y11zF5XSX595cDCsIr")
+ns = Netstorage("astin-nsu.akamaihd.net", "astinastin" ,"key")
 res = ns.download("/360949/rtmpplayer110.html")
 print(res.text)
