0876abedd1d1740b6440b5425e907cccfad88cad
astinchoi <asciineo@gmail.com>
2016-05-18 08:53:52 +0900

    change version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'r') as f:
 
 setup (
     name = 'netstorageapi',
-    version = '1.1.1',
+    version = '1.1.2',
     description = 'Akamai Netstorage API for Python',
     long_description = readme,
     packages = ['akamai'],
